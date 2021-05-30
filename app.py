71d83fcd9ff032fefa9918aac763826d64ad71bc
Maria Lobo <maria.lobo@awarelog.com>
2021-05-30 00:29:42 -0300

    fix front-back communication

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, Response
+from flask import Flask, request, jsonify
+from flask_cors import cross_origin
 from datetime import datetime
-import json
+
 from db import DB
 from domain.message import Message
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 
 
 @app.route('/message', methods=['POST'])
+@cross_origin()
 def send_message():
     json = request.json
     message = Message(
@@ -18,16 +20,18 @@ def send_message():
         json["author"]
     )
     DB.insert_message(message)
-    return Response(status=204)
+    return jsonify(status=204)
 
 
 @app.route('/feed/<int:patient_id>', methods=['GET'])
+@cross_origin()
 def get_feed(patient_id):
     # Retrieve all the feed of a patient, including all messages and medias
-    return Response(
+    message_list = [msg.to_json() for msg in DB.get_patient_messages(patient_id)]
+    return jsonify(
         status=200,
-        response=json.dumps([json.loads(msg.to_json()) for msg in DB.get_patient_messages(patient_id)]),
-        content_type='application/json'
+        response=message_list,
+        content_type='application/json',
     )
 
 
